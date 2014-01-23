64c59a298a2b562c3ac5b3cc9c30b61c141180d0
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-23 16:36:24 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OlympicsQuiz::Application.config.secret_key_base = '18362e419ec3f5efa0fd5d342eafa1189ec4fcb07e16c795753729ea089586af4de6e1dbc1d1c2750f1b0137b1e3c7ccdf507a77c3758be7d158b87f3f91d812'
